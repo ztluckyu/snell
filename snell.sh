04571d2a00c00ef7a6fb83c35c987d9dc0b5c70e
ztluckyu <110915816+ztluckyu@users.noreply.github.com>
2024-08-27 13:26:44 +0800

    Update snell.sh

--- snell.sh
+++ snell.sh
@@ -5,10 +5,10 @@ sudo apt-get update
 sudo apt-get install -y wget curl sudo vim git lsof mtr iperf3 unzip
 
 # 第二步
-wget https://dl.nssurge.com/snell/snell-server-v4.0.1-linux-amd64.zip
+wget https://dl.nssurge.com/snell/snell-server-v4.1.0b1-linux-amd64.zip
 
 # 第三步
-sudo unzip snell-server-v4.0.1-linux-amd64.zip -d /usr/local/bin
+sudo unzip snell-server-v4.1.0b1-linux-amd64.zip -d /usr/local/bin
 
 # 第四步
 sudo chmod +x /usr/local/bin/snell-server
